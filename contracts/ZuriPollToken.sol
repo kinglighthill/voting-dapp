a55ac7fefd51ffb1a13d03c0ffd44595153d0ae7
driphics <sodiqbuhari08@gmail.com>
2022-04-20 13:32:16 +0100

    ERC20 token created by drift

--- contracts/ZuriPollToken.sol
+++ contracts/ZuriPollToken.sol
@@ -1,16 +1,52 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract ZuriPollToken {
-    constructor() {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+contract ZuriPollToken is ERC20, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("ZuriPollToken", "MTK") ERC20Permit("ZuriPollToken") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    function mint() public {
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
 
-    } 
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
-    function buyToken() public {
-        
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
