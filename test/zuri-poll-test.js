2f83584f6781528be993b2e90d13f2b1fe7ec925
Olamigoke John <olamaigokejonny@gmail.com>
2022-04-22 00:28:41 -1200

    another passed test

--- test/zuri-poll-test.js
+++ test/zuri-poll-test.js
@@ -5,25 +5,21 @@ require("@nomiclabs/hardhat-waffle");
 describe("ZuriPoll", function () {
     let contract;
     let owner;
+    let zuriPoll;
+    let stakeholders;
    
       
 beforeEach(async function () {
  const ZuriPoll = await hre.ethers.getContractFactory("ZuriPollToken");
-
- const zuriPoll = await ZuriPoll.deploy("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
- await zuriPoll.deployed();
- 
-
  [owner, addr1, addr2] = await ethers.getSigners();
+zuriPoll = await ZuriPoll.deploy(owner.address);
+ await zuriPoll.deployed();
  
  });
 
  it("It should deploy successfully", async function (){
    console.log("success");
  });
-
- 
-
 
 
 
--- test/zuri-poll-token-test.js
+++ test/zuri-poll-token-test.js
@@ -6,17 +6,16 @@ const { string } = require("hardhat/internal/core/params/argumentTypes");
 require("@nomiclabs/hardhat-waffle"); 
 
    describe("ZuriPollToken", function () {
+     let contract;
+     let zuriPollToken;
       
          
    beforeEach(async function () {
     const ZuriPollToken = await hre.ethers.getContractFactory("ZuriPollToken");
-   
-    const zuriPollToken = await ZuriPollToken.deploy("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
-    await zuriPollToken.deployed();
-    
-
     [owner, addr1, addr2] = await ethers.getSigners();
-    
+    zuriPollToken = await ZuriPollToken.deploy(owner.address);
+    await zuriPollToken.deployed();
+          
     });
 
     it("It should deploy successfully", async function (){
@@ -24,7 +23,29 @@ require("@nomiclabs/hardhat-waffle");
     });
 
 
-    
+    it("it should deploy with total token", async function () {
+      const balance = await zuriPollToken.balanceOf(owner.address);
+      // expect(balance).to.equal(1000000);
+      expect(ethers.utils.formatEther(balance)).to.equal("1000000.0");
+      // console.log(ethers.utils.formatEther(balance));
+
+    });
+
+       
+
+    // it("Should set chairman", async function () {
+    //         buy = await zuriPollToken.connect(owner).buyToken();
+    //         // Expect the function to go through
+    //         const txResult = await buy.wait();
+    //         expect(txResult.status).to.equal(1);
+    //     });
+
+      // it("Should set chairman", async function () {
+      //       votingChairman = await votingContract.connect(admin).setChairman(chairman.address);
+      //       // Expect the function to go through
+      //       const txResult = await votingChairman.wait();
+      //       expect(txResult.status).to.equal(1);
+      //   });
     
   });
 
